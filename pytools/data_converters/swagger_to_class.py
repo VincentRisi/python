46411a217da1d78ab5d93b7c40822edd209010ac
vince <vincent.risi@gmail.com>
2026-01-01 14:43:57 +0200

    have a swagger stagger

--- pytools/data_converters/swagger_to_class.py
+++ pytools/data_converters/swagger_to_class.py
@@ -1,1 +1,34 @@
 import yaml
+import json
+import os
+
+import argparse
+arg_parser = argparse.ArgumentParser(
+    prog='swagger_to_class', 
+    description='Swagger or Openapi used to generate Python classes',
+    epilog='Bottom of help')
+arg_parser.add_argument('infile')
+arg_parser.add_argument('outfile')
+arg_parser.add_argument('-p', '--path', type=str)
+args = arg_parser.parse_args()
+
+def main():
+    if not os.path.exists(args.infile):
+        print (f'ERROR: {args.infile} does not exist.')
+        return False
+    path, extension = os.path.splitext(args.infile)
+    if not extension in ['.json', '.yaml']:
+        print (f'ERROR: {args.infile} is not JSON or YAML.')
+        return False
+    head, base = os.path.split(path)
+    with open(args.infile, 'r') as ifile:
+        data_string = ifile.read()
+    if extension in ['.json']:
+        data_dict = json.loads(data_string)
+    elif extension in ['.yaml']:
+        data_dict = yaml.safe_load(data_string)
+
+    return True
+
+if __name__ == '__main__':
+    main()
