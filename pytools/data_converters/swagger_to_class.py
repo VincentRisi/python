fc4efa48168b9839e2cac77b950f20504fa1c77d
vince <vincent.risi@gmail.com>
2026-01-07 17:10:00 +0200

    setattr work

--- pytools/data_converters/swagger_to_class.py
+++ pytools/data_converters/swagger_to_class.py
@@ -12,85 +12,36 @@ arg_parser.add_argument('infile')
 arg_parser.add_argument('outfile')
 arg_parser.add_argument('-p', '--path', type=str)
 args = arg_parser.parse_args()
-## print (args.path)
-
-## class Field:
-##     def __init__(self):
-##         self.name = ''
-##         self.type = None
-##         self.format = None
-##         self.minLength = None
-##         self.maxLength = None
-##         self.ref = None
-
-## class Struct:
-##     def __init__(self):
-##         self.name = ''
-##         self.isin = None
-##         self.required = []
-##         self.type = None
-##         self.ref = None
-##         self.fields = []
-
-## class Parameter:
-##     def __init__(self):
-##         self.name = ''
 
 def clear_ref(value):
     value = value.replace('#/definitions/','').replace('#/components/schemas/','')
     return value
 
-## def process_parameters(structures, parameters):
-##     for name in parameters:
-##         struct = Struct()
-##         struct.name = name
-##         schema = parameters[name]
-##         field = Field()
-##         struct.fields.append(field)
-##         field.name = schema['name'] if 'name' in schema else name
-##         struct.isin = schema['in'] if 'in' in schema else None
-##         struct.required = schema['required'] if 'required' in schema else None
+class Builder:
+    fields: dict
+    operationId: str
+    tags: list[str]
+    def __init__(self):
+        self.fields = dict()
+        self.operationId = ''
 
-## def process_paths(structures, paths):
-##     for command in paths:
-##         activity = paths[command]
-##         if not 'post' in activity:
-##             continue
-##         post = activity['post']
-##         operationId = post['operationId']
-##         if operationId != args.path:
-##             continue
-        
-## def process_definitions(structures, definitions):
-##     for def_name in definitions:
-##         struct = Struct()
-##         structures[def_name] = struct
-##         struct.name = def_name
-##         if 'required' in definitions[def_name]:
-##             for required in definitions[def_name]['required']:
-##                 struct.required.append(required)
-##         if 'type' in definitions[def_name]:
-##             struct.type = definitions[def_name]['type']
-##         if '$ref' in definitions[def_name]:
-##             struct.ref = clear_ref(definitions[def_name]['$ref'])
-##         if not 'properties' in definitions[def_name]:
-##             continue
-##         properties = definitions[def_name]['properties']
-##         for prop_name in properties:
-##             field = Field()
-##             field.name = prop_name
-##             struct.fields.append(field)
-##             for key in properties[prop_name]:
-##                 value = properties[prop_name][key]
-##                 if key == 'type': field.type = value
-##                 elif key == 'format': field.format = value
-##                 elif key == 'minLength': field.minLength = value
-##                 elif key == 'maxLength': field.maxLength = value
-##                 elif key == '$ref': field.ref = clear_ref(value)
+def get_field(data_dict, ref):
+    array = ref.split('/')
+    if len(array) == 3:
+        field = data_dict[array[1]][array[2]]
+    elif len(array) == 4:
+        field = data_dict[array[1]][array[2]][array[3]]
+    return field
 
+class Struct():
+    pass
+def recurse_field(builder, data_dict, field):
+    struct = Struct()
+    for key in field:
+        setattr(struct, key, field[key])
+    pass
 
-
-def load_data(data_dict):
+def load_data(builder, data_dict):
     paths = data_dict['paths']
     for key in paths:
         command_line = key
@@ -100,14 +51,17 @@ def load_data(data_dict):
         if not 'operationId' in ndict: continue
         operationId = ndict['operationId']
         if operationId != args.path: continue
-        tags = ndict['tags'] if 'tags' in ndict else None 
+        tags = ndict['tags'] if 'tags' in ndict else [] 
+        builder.operationId = operationId
+        builder.tags = tags
         parameters = ndict['parameters'] if 'parameters' in ndict else None
         for pdict in parameters:
             if '$ref' in pdict:
                 ref = pdict['$ref']
+                field = get_field(data_dict, ref)
+                recurse_field(builder, data_dict, field)
             else:
-                for field in pdict:
-                    pass
+                recurse_field(builder, data_dict, pdict)
         break
     pass    
 
@@ -126,7 +80,8 @@ def main():
         data_dict = json.loads(data_string)
     elif extension in ['.yaml']:
         data_dict = yaml.safe_load(data_string)
-    load_data(data_dict)
+    builder = Builder()
+    load_data(builder, data_dict)
 ##    structures = {}
 ##    if 'parameters' in data_dict:
 ##        parameters = data_dict['parameters']
