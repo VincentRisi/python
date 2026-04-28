e01251cc68a01b612da61490c29a47c1f0b4e17b
vincent risi <vincent.risi@gmail.com>
2026-04-28 06:17:58 +0200

    QUACK like a duck

--- modules/main/testers/encoders_test.cpp
+++ modules/main/testers/encoders_test.cpp
@@ -4,6 +4,7 @@ import std;
 import getargs;
 import encoders;
 import logfile;
+import tbuffer;
 
 static const char* logfile_name;
 static const char* binfile_name;
@@ -21,28 +22,28 @@ bool compare_buffers(unsigned char* buffer1, unsigned char* buffer2, long size)
 void base64_test(LogFile& log, long size, unsigned char* buffer)
 {
 	std::string encoded = base64_encode(buffer, size, Basic);
-  log.lprintf(eLogDebug, "%s", encoded.c_str());
+  log.lprintf(eLogDebug, "%ld %ld %s", size, encoded.length(), encoded.c_str());
 	std::string decoded = base64_decode(encoded, Basic);
 	bool equal = compare_buffers(buffer, (unsigned char*)decoded.c_str(), size);
   if (!equal)
-  {
     log.lprintf(eLogError, "Decoded buffer does not match original buffer for Basic encoding");
-	}
-  encoded = base64_encode(buffer, size, Mime);
-  log.lprintf(eLogDebug, "%s", encoded.c_str());
+  else
+		log.lprintf(eLogInfo, "Decoded buffer matches original buffer for Basic encoding");
+	encoded = base64_encode(buffer, size, Mime);
+  log.lprintf(eLogDebug, "%ld %ld %s", size, encoded.length(), encoded.c_str());
   decoded = base64_decode(encoded, Mime);
   equal = compare_buffers(buffer, (unsigned char*)decoded.c_str(), size);
   if (!equal)
-  {
-    log.lprintf(eLogError, "Decoded buffer does not match original buffer for Basic encoding");
-  }
+    log.lprintf(eLogError, "Decoded buffer does not match original buffer for Mime encoding");
+  else
+    log.lprintf(eLogInfo, "Decoded buffer matches original buffer for Mime encoding");
   encoded = base64_encode(buffer, size, URL_Safe);
-  log.lprintf(eLogDebug, "%s", encoded.c_str());
+  log.lprintf(eLogDebug, "%ld %ld %s", size, encoded.length(), encoded.c_str());
   decoded = base64_decode(encoded, URL_Safe);
   if (!equal)
-  {
-    log.lprintf(eLogError, "Decoded buffer does not match original buffer for Basic encoding");
-  }
+    log.lprintf(eLogError, "Decoded buffer does not match original buffer for URL_Safe encoding");
+  else
+    log.lprintf(eLogInfo, "Decoded buffer matches original buffer for URL_Safe encoding");
 }
 
 int main(int argc, char** argv)
@@ -65,7 +66,7 @@ int main(int argc, char** argv)
 	fseek(f, 0, SEEK_END);
 	long size = ftell(f);
 	fseek(f, 0, SEEK_SET);
-	unsigned char* buffer = new unsigned char[size];
+  TBUChar buffer(size);
 	fread(buffer, 1, size, f);
   fclose(f);
 	base64_test(log, 2000, buffer);
